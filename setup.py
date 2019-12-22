5c7f45d80df41d53bbd31270094bc44fd1d70e9f
Zeev Rotshtein <zeevro@gmail.com>
2019-12-22 17:19:27 +0200

    Better setup.py and remove win32_make.bat

--- setup.py
+++ setup.py
@@ -1,17 +1,8 @@
-﻿from distutils.core import setup, Extension
-from distutils.dir_util import remove_tree
-from distutils import ccompiler
-import os.path
+﻿try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils import setup, Extension
 
-c_args = []
-compiler = ccompiler.get_default_compiler()
-if compiler == 'unix':
-    c_args.append('-O3')
-
-src_path = os.path.join(os.path.dirname(__file__), 'cgeo.c')
-cgeo = Extension('cgeo', [src_path], extra_compile_args=c_args)
-
-setup(name='cgeo', version='1.0', ext_modules=[cgeo])
-
-if os.path.isdir("build"):
-    remove_tree("build", 1)
+setup(name='cgeo',
+      version='1.0',
+      ext_modules=[Extension('cgeo', ['cgeo.c'])])
--- win32_make.bat
+++
@@ -1,1 +0,0 @@
-@cl.exe cgeo.c /nologo /LD /Fecgeo.pyd /IC:\Python27\include /link /libpath:C:\Python27\libs\
