f0981176d16df33d47a81ca4f36fb2cc36ccabae
Zeev Rotshtein <zeevro@gmail.com>
2019-12-22 17:18:58 +0200

    Use unittest in test.py

--- test.py
+++ test.py
@@ -1,22 +1,30 @@
-from __future__ import print_function
-
-print()
-print('Testing...')
-print()
+import unittest
 
 import cgeo
 
-assert cgeo.great_circle_distance(1, 2, 3, 4) == 314.4915326116153
-assert cgeo.great_circle_distance(1, 2, 3, 4) == cgeo.great_circle_distance(3, 4, 1, 2)
 
-poly = ((0, 0),
-        (0, 2),
-        (2, 2),
-        (2, 0))
+class TestGreatCircleDistance(unittest.TestCase):
+    def test_literal(self):
+        self.assertEqual(cgeo.great_circle_distance(1, 2, 3, 4), 314.4915326116153)
 
-for x, y in [(1, 1), (0.5, 0.5), (1.5, 1.5)]:
-    assert cgeo.point_inside_polygon(x, y, poly)
-for x, y in [(1, 3), (3, 1), (-1, 1), (1, -1)]:
-    assert not cgeo.point_inside_polygon(x, y, poly)
+    def test_order(self):
+        self.assertEqual(cgeo.great_circle_distance(1, 2, 3, 4), cgeo.great_circle_distance(3, 4, 1, 2))
 
-print('Done.')
+
+class TestPointInPolygon(unittest.TestCase):
+    poly = ((0, 0),
+            (0, 2),
+            (2, 2),
+            (2, 0))
+
+    def test_inside(self):
+        for x, y in [(1, 1), (0.5, 0.5), (1.5, 1.5)]:
+            self.assertTrue(cgeo.point_inside_polygon(x, y, self.poly))
+
+    def test_outside(self):
+        for x, y in [(1, 3), (3, 1), (-1, 1), (1, -1)]:
+            self.assertFalse(cgeo.point_inside_polygon(x, y, self.poly))
+
+
+if __name__ == "__main__":
+    unittest.main()
