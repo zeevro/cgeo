4f560c2341f6bdb95bdeb7566e85a1d4660807c0
Zeev Rotshtein <zeev.rotshtein@perception-point.io>
2024-09-10 17:42:18 +0300

    Add warning when using slow fallback

--- cgeo/__init__.py
+++ cgeo/__init__.py
@@ -1,8 +1,9 @@
 try:
     from ._cgeo import great_circle_distance, point_inside_polygon
 except ImportError:
+    import warnings
+    warnings.warn('Could not import cgeo C extension, Using slow fallback.')
     from ._cgeo_slow import great_circle_distance, point_inside_polygon
-
 
 
 __all__ = ['great_circle_distance', 'point_inside_polygon']
