0de42e8890fa03f82b4d7c5bb7bbd9e034e8e96a
Zeev Rotshtein <zeevro@gmail.com>
2024-11-28 01:57:23 +0200

    Add type hints to test.py

--- test.py
+++ test.py
@@ -4,21 +4,21 @@ import cgeo
 
 
 class TestGreatCircleDistance(unittest.TestCase):
-    def test_literal(self):
+    def test_literal(self) -> None:
         self.assertAlmostEqual(cgeo.great_circle_distance(1, 2, 3, 4), 314.7551553654009)
 
-    def test_order(self):
+    def test_order(self) -> None:
         self.assertAlmostEqual(cgeo.great_circle_distance(1, 2, 3, 4), cgeo.great_circle_distance(3, 4, 1, 2))
 
 
 class TestPointInPolygon(unittest.TestCase):
     poly = ((0, 0), (0, 2), (2, 2), (2, 0))
 
-    def test_inside(self):
+    def test_inside(self) -> None:
         for x, y in [(1, 1), (0.5, 0.5), (1.5, 1.5)]:
             self.assertTrue(cgeo.point_inside_polygon(x, y, self.poly))
 
-    def test_outside(self):
+    def test_outside(self) -> None:
         for x, y in [(1, 3), (3, 1), (-1, 1), (1, -1)]:
             self.assertFalse(cgeo.point_inside_polygon(x, y, self.poly))
 
